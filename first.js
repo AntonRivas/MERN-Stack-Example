0f188cd09ec9833b1070aee10becf372d2954a1f
AntonRivas <antondearribas@gmail.com>
2021-05-16 18:20:43 +0100

    Readded packages

--- first.js
+++ first.js
@@ -1,9 +1,8 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('first.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+   res.send("Hello world! Beep Boop");
+});
+
+app.listen(8080);
